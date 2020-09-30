edd3e60bfdf4656371bbc6cb3745439d3dabdc12
Tobias Fischer <tobiasfischer17@gmail.com>
2020-09-30 04:06:42 -0400

    finished remote

--- remote.py
+++ remote.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from engines.engine_stretch import StretchEngine
 from utils import Tags, clear_screen, log
 from board import Board, Word
-import time, random, sys, logging
+import time, random, sys
 
 e = StretchEngine('v2')
 
@@ -14,8 +14,11 @@ INT_THRESH = 5
 def does_intersect(loc1, loc2):
     return abs(loc1['x'] - loc2['x']) < INT_THRESH and abs(loc1['y'] - loc2['y']) < INT_THRESH
 
-def gen_clue(turn):
+def gen_clue(turn, driver, swap_teams):
     try:
+        if swap_teams:
+            switch_teams(driver)
+
         cover_locs = []
         for cover in driver.find_elements_by_class_name('tokenWrapper'):
             cover_locs.append(cover.location)
@@ -45,29 +48,58 @@ def gen_clue(turn):
         print('\nThinking...')
         word, amnt = e.gen_word(board.get_summary(turn))
         print(f'\nYour clue is: {word}, {amnt}')
+
+        input_boxes = driver.find_elements_by_name('clue')
+        if len(input_boxes) == 0:
+            print('input box not found')
+            return
+        input_boxes[0].send_keys(word)
+
+        amount_box = driver.find_element_by_class_name('numberSelectWrapper')
+        amount_box.click()
+        [b for b in amount_box.find_elements_by_class_name('option') if b.text == str(amnt)][0].click()
+
+        time.sleep(0.5)
+        driver.find_element_by_class_name('jsx-1776081540').click()
+
     except Exception as ex:
         print(f'Exception occured in generating a clue -> {ex}')
 
-def main():
+def init(driver):
+    red_team_board = driver.find_element_by_id('teamBoard-red')
+    red_buttons = red_team_board.find_elements_by_class_name('jsx-198695588')
+    red_buttons[4].click()
+    print('inited to the read team')
+
+def switch_teams(driver):
+    driver.find_element_by_class_name('jsx-3037563900').click()
+    driver.find_element_by_class_name('jsx-445627889').click()
+
+def main(driver):
+    team_is_on = Tags.EMPTY
     while True:
-        turn = Tags.EMPTY
-        while True:
-            text = input('What would you like to do? ')
-            if 'red' in text.lower():
-                turn = Tags.RED
-                break
-            if 'blue' in text.lower():
-                turn = Tags.BLUE
-                break
-            if 'reset' in text.lower():
-                e.reset()
-                print('engine reset')
+        text = input('What would you like to do? ')
+        if 'red' in text.lower() or 'blue' in text.lower():
+            if team_is_on == Tags.EMPTY:
+                print('Please init first')
+            else:
+                turn = Tags.RED if 'red' in text.lower() else Tags.BLUE
+                gen_clue(turn, driver, team_is_on != turn)
+                team_is_on = turn
                 continue
-            if 'quit' in text.lower():
-                return
-            if 'given' in text.lower():
-                print(f'Given clues: {e.given_clues}')
-        gen_clue(turn)
+        if 'reset' in text.lower():
+            e.reset()
+            print('engine reset')
+            continue
+        if 'quit' in text.lower():
+            return
+        if 'given' in text.lower():
+            print(f'Given clues: {e.given_clues}')
+            continue
+        if 'init' in text.lower():
+            init(driver)
+            team_is_on = Tags.RED
+            continue
 
 if __name__ == "__main__":
     if not len(sys.argv) == 2:
@@ -85,4 +117,6 @@ if __name__ == "__main__":
 
         random.seed(100)
         log.verbosity = 0
-        main()
+
+        main(driver)
+        driver.close()
